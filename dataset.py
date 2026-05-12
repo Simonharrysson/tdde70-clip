2aa8941486f85f513a0389299b0ad21e61863a3e
Emma <emmabertmar@gmail.com>
2026-05-12 15:43:14 +0200

    datafixes

--- dataset.py
+++ dataset.py
@@ -1,3 +1,5 @@
+import json
+import random
 from torch.utils.data import Dataset, DataLoader
 from PIL import Image
 
@@ -15,6 +17,27 @@ class RSICDDataset(Dataset):
         f, cap = self.samples[idx]
         img = self.preprocess(Image.open(self.image_dir / f).convert("RGB"))
         return img, self.tokenizer([cap])[0]
+
+def load_split_b(json_path, val_ratio=0.1, seed=42):
+    with open(json_path) as f:
+        data = json.load(f)
+
+    images = [img for img in data["images"] if img["split"] == "val"]
+    random.seed(seed)
+    random.shuffle(images)
+
+    split = int((1 - val_ratio) * len(images))
+    train_images = images[:split]
+    val_images   = images[split:]
+
+    train_samples = [
+        (img["filename"], sentence["raw"])
+        for img in train_images
+        for sentence in img["sentences"]
+    ]
+    val_filenames = [img["filename"] for img in val_images]
+
+    return train_samples, val_filenames
 
 def make_loader(samples, image_dir, tokenizer, preprocess, batch_size):
     return DataLoader(RSICDDataset(samples, image_dir, tokenizer, preprocess),
